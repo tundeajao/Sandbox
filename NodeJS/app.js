1bdcb1784b29500ea1e388d6dd853a9f0a6a2dc6
Dr Tunde Ajao <tunde_ajao@hotmail.com>
2022-09-29 02:38:57 +0100

    Objects and Parameters

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -14,6 +14,7 @@ SendGreet(Greet);
 greet2();
 **/
 
+/**
 person = {
     firstname: 'John',
     lastname: 'Doe',
@@ -25,3 +26,23 @@ person = {
 person.greet();
 
 console.log(person['firstname']);
+**/
+
+function Person (firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
+
+Person.prototype.greet = function() {
+    console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+}
+
+var john = new Person('John', 'Peter');
+john.greet();
+
+var jane = new Person('Jane', 'Doe');
+jane.greet();
+
+console.log(john.__proto__);
+console.log(jane.__proto__);
+console.log(john.__proto__ === jane.__proto__);
