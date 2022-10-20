569db6c146c976762aa4e4c149e52ea9689f424d
Tunde Ajao <tunde_ajao@hotmail.com>
2022-10-20 21:04:45 +0100

    Updating greet folders

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,4 +1,22 @@
-var greet = require('./greet');
+var greet = require('./greet1');
+greet();
 
-greet.english();
-greet.spanish();
+// var greet2 = require('./greet2');
+// greet2.greet();
+
+var greet2 = require('./greet2').greet;
+greet2();
+
+var greet3 = require('./greet3');
+greet3.greet();
+greet3.greeting = 'Changed Hello World';
+
+var greet3b = require('./greet3');
+greet3b.greet();
+
+var greet4 = require('./greet4');
+var grtr = new greet4();
+grtr.greet();
+
+var greet5 = require('./greet5.js');
+greet5.greet();
